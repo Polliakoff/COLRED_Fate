85691a67e8172b29f11ae977f139dd82035a50fd
Polyakov Daniil <70587587+Polliakoff@users.noreply.github.com>
2022-05-05 17:29:32 +0300

    new_mpdels_old_deleted

--- main/models.py
+++ main/models.py
@@ -13,54 +13,57 @@ class Avatar_of_choice(models.Model):
 
 class Character(models.Model):
     name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=10000, default="", blank = True)
     portrait = models.ImageField(upload_to ='user_images/',blank = True)
-    custom_avatar = models.BooleanField(default=False)
+    custom_avatar = models.BooleanField(default=False,blank = True)
     usr = models.ForeignKey(User, on_delete=models.CASCADE)
-    chr_class = models.CharField(max_length=13,null=TRUE,blank=TRUE)
-    xp = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    background = models.CharField(max_length=100,null=TRUE,blank=TRUE)
-    diety = models.CharField(max_length=100,null=TRUE,blank=TRUE)
-    origin = models.CharField(max_length=100,null=TRUE,blank=TRUE)
-    worldview = models.CharField(max_length=2,null=TRUE,blank=TRUE)
-    attrib_strength = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    attrib_agility = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    attrib_charisma = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    attrib_endurance = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    attrib_intelligence = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    attrib_wisdom = models.IntegerField(null=TRUE,blank=TRUE,default=10)
-    armour_class = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    hp = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    speed = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_acrobatics = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_arcane = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_athletics = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_craft = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_decieve = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_diplomacy = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_intimidation = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_medicine = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_nature = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_occult = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_performance = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_religion = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_social = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_stealth = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_survival = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    skills_theft = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    save_resilience = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    save_reflex = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    save_resilience = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    save_will = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    weapon = models.CharField(max_length=100, null=TRUE,blank=TRUE) 
-    attack = models.CharField(max_length=100, null=TRUE,blank=TRUE)
-    damage = models.CharField(max_length=100, null=TRUE,blank=TRUE) 
-    attention = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    dc_class = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    notes = models.TextField(null=TRUE,blank=TRUE)
-    is_dying = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-    mortal_wounds = models.IntegerField(null=TRUE,blank=TRUE,default=0)
-
+    high_concept = models.CharField(max_length=1000, default="", blank = True)
+    trouble = models.CharField(max_length=1000, default="", blank = True)
+    max_skill = models.IntegerField(default=5, blank = True)
+    min_skill = models.IntegerField(default=1, blank = True)
+    fate_point_number = models.IntegerField(default=3, blank = True)
+    fate_point_refresh = models.IntegerField(default=3, blank = True)
     def __str__(self):
         return self.name
+
+
+class aspect(models.Model):
+    desc = models.CharField(max_length=1000)
+    chr = models.ForeignKey('Character', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.desc
+
+class skill(models.Model):
+    name = models.CharField(max_length=100,default = "")
+    level = models.IntegerField(default = 0, blank=True)
+    chr = models.ForeignKey('Character', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class stunt(models.Model):
+    name = models.CharField(max_length=100, default = "")
+    desc = models.CharField(max_length=10000, default = "", blank=True)
+    chr = models.ForeignKey('Character', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class extra(models.Model):
+    name = models.CharField(max_length=100, default = "")
+    cost = models.CharField(max_length=100, default = "", blank=True)
+    attached_aspects = models.CharField(max_length=10000, default = "", blank=True)
+    attached_stunts = models.CharField(max_length=10000, default = "", blank=True)
+    attached_skills = models.CharField(max_length=10000, default = "", blank=True)
+    details = models.CharField(max_length=10000, default = "", blank=True)
+    chr = models.ForeignKey('Character', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class consequence(models.Model):
+    severity = models.CharField(max_length=100, default = "")
+    desc = models.CharField(max_length=10000, default = "", blank=True)
+    chr = models.ForeignKey('Character', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.desc
+
 
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import Image_Upload_Form, Image_Upload_Form_ava_edition, psw_ch
-from .forms import Great_List_Form, Image_Upload_Form
+from .forms import Image_Upload_Form
 from django.contrib.auth import update_session_auth_hash
 from .models import Character, Avatar_of_choice
 
@@ -105,7 +105,6 @@ def redactor(request,usr_id,chr_id):
         path_string = '/media/' + str(av.portrait)
     else:
         path_string = '/static/main/images/avatars/blank.png'
-    chrctrs = request.user.character_set.all()
     current_character = Character.objects.get(id = chr_id)
     
     if current_character.custom_avatar:
@@ -117,55 +116,21 @@ def redactor(request,usr_id,chr_id):
         return redirect('/') 
 
     if request.method == 'POST':
-        sent_gerat_form = Great_List_Form(request.POST)
         sent_image_form = Image_Upload_Form(request.POST)
-        
-        if sent_gerat_form.is_valid():
-            current_character.name = request.POST.get('name')
-            current_character.attrib_strength = request.POST.get('attrib_strength')
-            current_character.attrib_agility = request.POST.get('attrib_agility')
-            current_character.attrib_charisma = request.POST.get('attrib_charisma')
-            current_character.attrib_endurance = request.POST.get('attrib_endurance')
-            current_character.attrib_intelligence = request.POST.get('attrib_intelligence')
-            current_character.attrib_wisdom = request.POST.get('attrib_wisdom')
-            current_character.background = request.POST.get('background')
-            current_character.diety = request.POST.get('diety')
-            current_character.origin = request.POST.get('origin')
-            current_character.worldview = request.POST.get('worldview')
-            current_character.xp = request.POST.get('xp')
-            current_character.chr_class = request.POST.get('chr_class')
-            current_character.hp = request.POST.get('hp')
-            current_character.armour_class = request.POST.get('armour_class')
-            current_character.speed = request.POST.get('speed')
-            current_character.is_dying = request.POST.get('is_dying')
-            current_character.mortal_wounds = request.POST.get('mortal_wounds')
-
-
-            current_character.save()
-            return redirect('/redactor/'+str(request.user.id)+'/'+str(current_character.id))
-
         if sent_image_form.is_valid():
             current_character.portrait = request.FILES['portrait']
             current_character.custom_avatar = True
             current_character.save()
             return redirect('/redactor/'+str(request.user.id)+'/'+str(current_character.id))
 
-        print(str(sent_image_form.errors))
-        print(sent_image_form.non_field_errors)
-
- 
-
-    form = Great_List_Form(instance=current_character)
     portrait_form = Image_Upload_Form(instance=current_character)
     
     cont = {
         'username' : request.user.username,
         'avatar' : path_string,
         'chr_portrait': path_string_chr, 
-        'form' : form,
         'portrait_form' : portrait_form,
         'current_character': current_character,
-        'characters': chrctrs,
     }
     return render(request, 'main/list.html', cont)
 
